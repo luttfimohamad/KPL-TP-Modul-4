206ca671c2af934a82dd137d57a6004be09b3356
luttfimohamad <luttfimohamad@gmail.com>
2022-03-16 23:54:21 +0700

    Add project files.

---
+++ tpmodul4_1302204066/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Samlikum");
+        }
+    }
+}
